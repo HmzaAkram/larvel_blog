e6fb856c06ad2ce1768d8a537a501dda99306c97
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-04 04:24:22 +0500

    working

--- larvel_blogs/resources/views/components/form-alerts.blade.php
+++ larvel_blogs/resources/views/components/form-alerts.blade.php
@@ -1,3 +1,30 @@
 <div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+   <div class="mb-3">
+      @if (session('info'))
+         <div class="alert alert-info">
+            {{ session('info') }}
+            <button class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+      @endif
+
+      @if (session('fail'))
+         <div class="alert alert-danger"> <!-- Fixed "alert-dangar" typo -->
+            {{ session('fail') }}
+            <button class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span> <!-- Fixed "6times;" -->
+            </button>
+         </div>
+      @endif
+
+      @if (session('success'))
+         <div class="alert alert-success">
+            {{ session('success') }}
+            <button class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+      @endif
+   </div>
 </div>
