36b3757194bba274ee453fd3f504d3425b7d0bf3
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-04 02:09:42 +0500

    Update web.php

--- larvel_blogs/routes/web.php
+++ larvel_blogs/routes/web.php
@@ -10,3 +10,12 @@ Route::get('/', function () {
 
 // Admin Routes
 
+Route::prefix('admin')->name('admin')->group(function (Request $request){
+    Route::middleware([])->group(function(){
+           Route::controller(AuthController::class)->group(function(){
+            Route::get('/login','loginForm')->name(login);
+            Route::get('/forgot-Password')->name(forgot);
+           }
+    );
+    });
+});
