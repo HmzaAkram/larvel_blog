154d9afe9df60a7b3684fd2d7ac3e3f75ef53437
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-04 01:58:46 +0500

    Update web.php

--- larvel_blogs/routes/web.php
+++ larvel_blogs/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controller\AuthController;
+use app\Http\Controller\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
