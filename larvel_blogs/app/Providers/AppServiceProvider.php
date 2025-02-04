c5e0f2b326b78ca6ec855d66a2f2e2fc7301d549
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-05 02:45:07 +0500

    Update AppServiceProvider.php

--- larvel_blogs/app/Providers/AppServiceProvider.php
+++ larvel_blogs/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Session;
 
 class AppServiceProvider extends ServiceProvider
 {
