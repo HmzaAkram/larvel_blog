24cf71fbad110eecd21faa819656f3524a91ab35
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-05 03:59:56 +0500

    Update CMail.php

--- larvel_blogs/app/Helpers/CMail.php
+++ larvel_blogs/app/Helpers/CMail.php
@@ -1,8 +1,50 @@
 <?php
 
 namespace App\Helpers;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-Class CMAIL{
-    
+Class CMail 
+{
+  public static function sendEmail($config){
+    //Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      
+    $mail->isSMTP();                                            
+    $mail->Host       = config('services.mail.host');                     
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = config('services.mail.username');                     
+    $mail->Password   = config('service.mail.password');                       
+    $mail->SMTPSecure = config('service.mail.encrption');           
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+  }
 }
 ?>
