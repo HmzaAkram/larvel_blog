b7a520e38f06c2e7ee18dc0f9ef0cbe202bf130a
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-04 23:45:04 +0500

    Update AuthController.php

--- larvel_blogs/app/Http/Controllers/AuthController.php
+++ larvel_blogs/app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\UserStatus;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -22,6 +25,7 @@ class AuthController extends Controller
     }
     
     public function loginHandler(Request $request){
-         dd($request->all());
+         $fieldType = ilter_var($request->login_id, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+         dd($fieldType);
     }
 }
