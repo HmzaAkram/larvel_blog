0cac526786c8ab73eb15545905daf3faeb92e071
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-06 00:37:01 +0500

    Update forgot-template.blade.php

--- larvel_blogs/resources/views/email-templates/forgot-template.blade.php
+++ larvel_blogs/resources/views/email-templates/forgot-template.blade.php
@@ -1,5 +1,4 @@
-<?php
-
+<!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -53,9 +52,9 @@
     <div class="container">
         <h2>Password Reset Request, {{ $user->name }}</h2>
         <p>We received a request to reset your password. Click the button below to set a new password.</p>
-        <a href="{{$actionLink}}" target="_blank" class="btn">Reset Password</a>
+        <a href="{{ $actionLink }}" target="_blank" class="btn">Reset Password</a>
         <p>If you did not request a password reset, please ignore this email.</p>
-        <p class="footer">{{date('Y')}}  © 2025 larablog. All rights reserved.</p>
+        <p class="footer">{{ now()->year }} © larablog. All rights reserved.</p>
     </div>
 </body>
 </html>
