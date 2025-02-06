f19d00fe5d697c4a43436c523b89895bf3d00823
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-07 01:05:32 +0500

    Update AuthController.php

--- larvel_blogs/app/Http/Controllers/AuthController.php
+++ larvel_blogs/app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use App\UserStatus;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Carbon;
 use App\Helpers\CMail; // Corrected Mail facade import
