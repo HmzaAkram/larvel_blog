428c46b9146254d428f619872ebf75c849fa1df4
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-07 00:22:05 +0500

    Update CMail.php

--- larvel_blogs/app/Helpers/CMail.php
+++ larvel_blogs/app/Helpers/CMail.php
@@ -1,50 +1,50 @@
 <?php
 
 namespace App\Helpers;
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-Class CMail 
+class CMail
 {
-  public static function sendEmail($config){
-    //Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+    public static function sendEmail($config)
+    {
+        $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      
-    $mail->isSMTP();                                            
-    $mail->Host       = config('services.mail.host');                     
-    $mail->SMTPAuth   = true;                                   
-    $mail->Username   = config('services.mail.username');                     
-    $mail->Password   = config('service.mail.password');                       
-    $mail->SMTPSecure = config('service.mail.encrption');           
-    $mail->Port       = config('service.mail.port');                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        try {
+            // Server settings
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host       = config('services.mail.host');
+            $mail->SMTPAuth   = true;
+            $mail->Username   = config('services.mail.username');
+            $mail->Password   = config('services.mail.password');
+            $mail->SMTPSecure = config('services.mail.encryption');
+            $mail->Port       = config('services.mail.port');
 
-    //Recipients
-    $mail->setFrom(
-        isset($config['from_address']) ? $config['from_address'] : config('service.mail.from_address'),
-        isset($config['from_name'])? $config['from_name'] : config('service.mail.from_name')
-    );
-    $mail->addAddress($config['recipient_address'], isset($config['recipient_name']) 
-    ?
-    $config['recipient_name'] : null);    
+            // Set From Address
+            $fromAddress = $config['from_address'] ?? config('services.mail.from_address');
+            $fromName = $config['from_name'] ?? config('services.mail.from_name');
+            $mail->setFrom($fromAddress, $fromName);
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $config['subject'];
-    $mail->Body    = $config['body'];
+            // Recipient
+            $recipientAddress = $config['recipient_address'];
+            $recipientName = $config['recipient_name'] ?? null;
+            $mail->addAddress($recipientAddress, $recipientName);
 
-    if($mail->send()){
-        return false; 
-    }else{
-        return true;  // Return true if email sent successfully
+            // Email Content
+            $mail->isHTML(true);
+            $mail->Subject = $config['subject'];
+            $mail->Body    = $config['body'];
+
+            if ($mail->send()) {
+                return true; // Email sent successfully
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            return 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+        }
     }
-    
-} catch (Exception $e) {
-   return false;
-}
-
-  }
 }
 ?>
