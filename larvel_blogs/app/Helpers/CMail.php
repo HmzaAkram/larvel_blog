fd460eca0c9afd86775e7392ed145622d7886541
Itxhamza7 <hmzaakram295@gmail.com>
2025-02-07 00:53:32 +0500

    Update CMail.php

--- larvel_blogs/app/Helpers/CMail.php
+++ larvel_blogs/app/Helpers/CMail.php
@@ -4,6 +4,7 @@ namespace App\Helpers;
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use Illuminate\Support\Facades\Log;
 
 class CMail
 {
@@ -27,23 +28,47 @@ class CMail
             $fromName = $config['from_name'] ?? config('services.mail.from_name');
             $mail->setFrom($fromAddress, $fromName);
 
+            // Set Reply-To (Optional)
+            if (!empty($config['reply_to_address'])) {
+                $mail->addReplyTo($config['reply_to_address'], $config['reply_to_name'] ?? '');
+            }
+
             // Recipient
+            if (empty($config['recipient_address'])) {
+                throw new Exception("Recipient address is required.");
+            }
+
             $recipientAddress = $config['recipient_address'];
-            $recipientName = $config['recipient_name'] ?? null;
+            $recipientName = $config['recipient_name'] ?? '';
             $mail->addAddress($recipientAddress, $recipientName);
 
             // Email Content
+            if (empty($config['subject']) || empty($config['body'])) {
+                throw new Exception("Email subject and body are required.");
+            }
+
             $mail->isHTML(true);
             $mail->Subject = $config['subject'];
             $mail->Body    = $config['body'];
 
+            // Attachments (Optional)
+            if (!empty($config['attachments']) && is_array($config['attachments'])) {
+                foreach ($config['attachments'] as $attachment) {
+                    if (file_exists($attachment)) {
+                        $mail->addAttachment($attachment);
+                    }
+                }
+            }
+
+            // Send email
             if ($mail->send()) {
-                return true; // Email sent successfully
+                return true;
             } else {
-                return false;
+                throw new Exception("Email sending failed.");
             }
         } catch (Exception $e) {
-            return 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+            Log::error("Email Error: " . $e->getMessage());
+            return false;
         }
     }
 }
