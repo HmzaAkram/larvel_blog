55f6355c501cf89b32d7f918566dd5f0ed45e2b4
Itxhamza7 <hmzaakram295@gmail.com>
2025-03-15 16:14:26 +0500

    pta nhi kya ho rha ha

--- larvel_blogs/resources/views/back/pages/auth/login.blade.php
+++ larvel_blogs/resources/views/back/pages/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('back.layout.auth-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title Here')
 @section('content')
-<div class="login-box bg-white box-shadow border-radius-10">
+ <div class="login-box bg-white box-shadow border-radius-10">
 							<div class="login-title">
 								<h2 class="text-center text-primary">Login</h2>
 							</div>
