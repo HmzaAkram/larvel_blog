461c0f08253b0c05e5b5de98df17c62cc2e213bc
Itxhamza7 <hmzaakram295@gmail.com>
2025-03-15 15:31:12 +0500

    wokin hard

---
+++ larvel_blogs/resources/views/front/partails/header.blade.php
@@ -0,0 +1,21 @@
+<div class="header_section">
+   <div class="container-fluid header_main">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <a class="logo" href="{{ url('/') }}"><img src="images/logo.png"></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+               <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
+               <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
+               <li class="nav-item"><a class="nav-link" href="{{ url('/blog') }}">Blog</a></li>
+               <li class="nav-item"><a class="nav-link" href="{{ url('/features') }}">Features</a></li>
+               <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">Contact Us</a></li>
+               <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+               <li class="nav-item"><a class="nav-link" href="#"><img src="images/serach-icon.png"></a></li>
+            </ul>
+         </div>
+      </nav>
+   </div>
+</div>
